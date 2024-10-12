3960628098741f0dadc2c8473920a0371865d53e
Matt Kollada <mattkollada@gmail.com>
2024-10-12 00:22:29 -0400

    updating with emmas designs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"], // Add Inter as the default sans-serif font
+      },
+    },
   },
   plugins: [],
 };
