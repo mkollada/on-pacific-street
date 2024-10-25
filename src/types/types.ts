1b5bd0f4fb0ebbf0416be690c49a1bea69bda833
Matt Kollada <mattkollada@gmail.com>
2024-10-24 22:33:31 -0400

    fixed type issue in full performance page

---
+++ src/types/types.ts
@@ -0,0 +1,14 @@
+// types.ts
+
+export interface Performance {
+  artistName: string;
+  youtubeLink?: string;
+  date: string;
+  about?: string;
+  instaHandle?: string;
+  spotifyId?: string;
+  bandcampLink?: string;
+  audioEngineers?: string[];
+  videographers?: string[];
+  MC?: string;
+}
